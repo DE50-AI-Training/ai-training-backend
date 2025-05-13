6f2c19be19b42d313a5db9df3b707b0e94cd8dca
Wiqiro <william.imbert@utbm.fr>
2025-05-13 17:09:07 +0200

    feat: added redis for future needs

--- src/api/main.py
+++ src/api/main.py
@@ -1,19 +1,23 @@
 from contextlib import asynccontextmanager
 
+import redis
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import SQLModel
 
 from api.database import engine
 from api.routers import datasets, models
+from config import settings
 
 
+# Create the database engine
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     SQLModel.metadata.create_all(engine)
     yield
 
 
+# Start the FastAPI app
 app = FastAPI(lifespan=lifespan, docs_url="/")
 app.include_router(models.router)
 app.include_router(datasets.router)
@@ -28,3 +32,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Initialize redis
+redis = redis.Redis.from_url(settings.redis_url, decode_responses=True)
--- src/config.py
+++ src/config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     uvicorn_workers: int = Field(1, env="UVICORN_WORKERS")
     port: int = Field(8000, env="PORT")
     database_url: str = Field("sqlite:///database.db", env="DATABASE_URL")
+    redis_url: str = Field("redis://localhost:6379", env="REDIS_URL")
 
     class Config:
         env_file = ".env"
