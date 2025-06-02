68f932a744709ac84539d8c56257fe7d5b692206
Mino1289 <kretzvictor@orange.fr>
2025-06-02 23:21:02 +0200

    test(infer): Adding test for inference & edit of run_classification to match with inference. inference is not dependant of an architecture

---
+++ tests/run_inference_MLP.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
+
+import pandas as pd
+
+from trainer.infer import infer_on_dataset  
+
+# chargement du dataset + adaptation de la config
+df = pd.read_csv("tests/iris.csv")
+
+config = {
+    "csv_path": "tests/iris.csv",
+    "input_columns": [0, 1, 2, 3],
+    "column_name": ["result"],
+    "classification": True,  # Set to True for classification tasks
+    "classes": ["Iris-setosa", "Iris-versicolor", "Iris-virginica"],
+    "separator": ",",
+    "model_arch_path": "saved_models/iris_run5/model.json",
+    "batch_size": 16,
+    "cleaning": False,
+    "seed": 42,
+    "device": "cpu",
+    "save_dir": "saved_models/iris_run5",
+    "image_column": None
+}
+
+infer_on_dataset(config)
+# The inference results will be saved in the specified directory.
