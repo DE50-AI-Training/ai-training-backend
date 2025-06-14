a00562ab52343ee0a609ccfe5a7509d64f8dec86
Wiqiro <williamnoimbert@gmail.com>
2025-06-14 23:36:56 +0200

    feat: start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Démarrer Celery worker en arrière-plan
-cd /app/src
-celery -A trainer.trainer:app worker --loglevel=info &
-
-# Démarrer FastAPI
-python -m uvicorn api.main:app --host 0.0.0.0 --port 8000
